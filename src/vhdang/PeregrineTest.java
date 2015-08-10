72f84d40e680a0f3a5558535107574e35bfba028
ruaxauxi <ruaxauxi@yahoo.com>
2015-08-10 11:47:21 +0200

    initial commit

---
+++ src/vhdang/PeregrineTest.java
@@ -0,0 +1,21 @@
+/**
+ * 
+ */
+package vhdang;
+
+/**
+ * @author haidangvo
+ *
+ */
+public class PeregrineTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hi!");
+
+	}
+
+}
